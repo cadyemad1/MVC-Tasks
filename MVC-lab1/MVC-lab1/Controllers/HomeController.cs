dea1fd52b116785c42e87ca4226eba7f70b6f8b5
Cady Emad <cady.emad@stud.fci-cu.edu.eg>
2020-05-04 16:42:03 +0200

    Add files via upload

---
+++ MVC-lab1/MVC-lab1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //class employee
+        public class User
+        {
+            public int Id { get; set; }
+            public String Name { get; set; }
+            public String Email { get; set; }
+            public String Phone { get; set; }
+        }
+
+      [HttpPost]
+        public ViewResult Register(User user)
+        {
+            if (user != null && user.Name != null && user.Email != null)
+            {
+                ViewBag.Name = user.Name;
+                return View("Welcome");
+
+            }
+
+            return View();
+
+        }
+
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC-lab1/MVC-lab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_lab1.MvcApplication" Language="C#" %>
